0d577fd3fcaa22a55190ca84190636bb02d1dac7
Fowgard <bilidanek@gmail.com>
2018-09-27 16:19:17 +0200

    Osetreni argumentu

--- main.c
+++ main.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 
+#define FILE_ERROR 5
+
 int main(int argc, char *argv[])
 {
+	FILE *f;
+
+	if (argc != 2) 
+	{
+		fprintf(stderr, "Nesparvny pocet argumentu.");
+		exit(FILE_ERROR);
+	}
+	
+	if (f = fopen(argv[1], "r") == NULL)
+	{	
+		fprintf(stderr, "Nepodarilo se otevrit soubor.");
+		exit(FILE_ERROR);
+	}
+
+	
 	printf("%s\n", "HELL");
 
 	return 0;
