793d6c7e56be31700d5b1527b89d07a064031247
Fowgard <bilidanek@gmail.com>
2018-11-27 19:11:01 +0100

    generator - pokracovani

--- generator.c
+++ generator.c
@@ -16,6 +16,20 @@
 		CAT_STR(buffer);\
 	}while(0) \
 
+#define CAT_FLOAT(num) \
+	do{ \
+		char buffer[310];	\
+		sprintf(buffer, "%a", num);	\
+		CAT_STR(buffer);\
+	}while(0) \
+
+#define CAT_ESC_SEQUENCE(symbol) \
+	do{ \
+		char buffer[310];	\
+		sprintf(buffer, "%03d", symbol);	\
+		CAT_STR(buffer);\
+	}while(0) \
+
 lexem_t *code_main;
 lexem_t *code_rest;
 
@@ -41,31 +55,33 @@ void gen_while_start(int condition)
 void gen_frame_retvar()
 {
 	CAT_INST("CREATEFRAME");
-	CAT_INST("DEFVAR TF@!retvar");
+	CAT_INST("PUSHFRAME");
+	CAT_INST("DEFVAR LF@!retvar");// MUSI BYT NA LF PROTOZE PO KONCI FUNKCI SE DELA POPFRAME,
+								// VEDLO BY KE ZTRATE HODNOTY
 
 }
 
 void gen_inputi()
 {
 	gen_frame_retvar();
-	CAT_INST("READ TF@!retvar int");
+	CAT_INST("READ LF@!retvar int");
 
-	CAT_INST("PUSHFRAME");
+	CAT_INST("POPFRAME");
 	CAT_INST("RETURN");		
 }
 
 void gen_inputf()
 {
 	gen_frame_retvar();
-	CAT_INST("READ TF@!retvar float");
-	CAT_INST("PUSHFRAME");
+	CAT_INST("READ LF@!retvar float");
+	CAT_INST("POPFRAME");
 	CAT_INST("RETURN");		
 }
 void gen_inputs()
 {
 	gen_frame_retvar();
-	CAT_INST("READ TF@!retvar string");
-	CAT_INST("PUSHFRAME");
+	CAT_INST("READ LF@!retvar string");
+	CAT_INST("POPFRAME");
 	CAT_INST("RETURN");		
 }
 void print_output()
@@ -99,7 +115,7 @@ void generator_init()
 		exit(-1);
 	}
 
-	pomocna_docasna_funkce();
+	//pomocna_docasna_funkce();
 
 	
 }
@@ -143,6 +159,53 @@ void gen_main_end()
 	CAT_INST("");
 }
 
+void gen_var_from_token(token_t *token)
+{
+	if(token->type == TYPE_IDENTIFIER)
+	{
+		CAT_STR("LF@!");
+		CAT_STR(token->attribute.string.word);
+	}
+	else if(token->type == TYPE_INT)
+	{
+		CAT_STR("int@");
+		CAT_NUM(token->attribute.integer);
+	}
+	else if(token->type == TYPE_FLOAT)
+	{
+		CAT_STR("float@");
+		CAT_FLOAT(token->attribute.decimal);
+	}
+	else if(token->type == TYPE_STRING)
+	{
+		CAT_STR("string@");
+		int i = 0;
+		char symbol = token->attribute.string.word[i]; 
+		while(symbol != '\0')
+		{
+			if (symbol == '#' || symbol == '\\' || symbol < 33 || (!isprint(symbol)))
+			{
+				CAT_STR("\\");
+				CAT_ESC_SEQUENCE(token->attribute.string.word[i]);
+			}
+			else
+			{
+				(gen_to_main == 1) ? lexem_putchar(code_main, symbol) : lexem_putchar(code_rest, symbol);
+			}
+
+			i++;
+			symbol = token->attribute.string.word[i];
+		}
+	}
+	else
+	{
+		//error
+
+	}
+}
+
+
+
 
 void gen_def_start(char *f_name)
 {
@@ -160,13 +223,35 @@ void gen_def_start(char *f_name)
 
 }
 
+void gen_def_return()//nemusi byt vzdy na konci = proto zvlast
+{
+	CAT_INST("MOVE LF@!retvar LF@!result");
+	CAT_INST("POPFRAME");
+	CAT_INST("RETURN");
+}
+
 void gen_def_end(char *f_name)
 {
 	//CAT_STR("LABEL $"); CAT_STR(function_id); CAT_STR("%return\n"); na co?
 	CAT_INST("POPFRAME");
 	CAT_INST("RETURN");
 }
+void create_frame()//musi se vyrvorit frame pro parametry, pro lepsi prehlednost pri volani z parseru
+{
+	CAT_INST("CREATEFRAME");
+}
 
+void gen_call(char *fname)
+{
+	CAT_STR("CALL $");
+	CAT_STR(fname);
+	CAT_INST("");
+}
 
-
+void gen_assign_from_call(char *v_name)
+{
+	CAT_STR("MOVE LF@!");
+	CAT_STR(v_name);
+	CAT_INST(" TF@!retvar");
+}
 
