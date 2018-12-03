4d9c84e2914c22c7b40b3937ae155293320db9da
Unknown <xxxuba@seznam.cz>
2018-12-03 10:17:49 +0100

    frida chrry pick

--- parser.c
+++ parser.c
@@ -11,7 +11,7 @@ int param_counter; //pocitadlo parametru pri definici funkce
 int porovnavani_counter;//pocitadlo >,<,<=,>=,==,!=
 bool already_init=false;//podmínka pro inicializaci parseru
 int uvnitr_funkce=0;//pro orientaci jestli jsme v definic funkce nebo ne 
-int typ_promene;//typ promene
+int typ_promene=WITHOUT_TYPE;//typ promene
 bool uzavorkovana_funkce=false; // pro volani funkce flag na poznani jestli je funkce uzavorkovana nebo ne 
 int pocitac_param_u_call=0;//pocitadlo argumentu pro volani funkce
 bool last_id_func;//jestli poslední identifikator byla funkce nebo promena
@@ -395,12 +395,26 @@ int rule_expresion_pusher(){
 	SInit(&tmp_s);
 	int result=NO_ERROR;
 	int p_tok1;
-
 	int top_stack = I_DOLLAR;
 	SPush(&stack, top_stack);
 
 	bool is_last_NONTERM = false;
 	p_tok1 = get_prec_table_index(token_type);
+	printf("token TYPE na ZACATKU :::::::: %d \n",token_type );
+/*frida*/	if (token_type!=TYPE_IDENTIFIER && (token_type>=TYPE_INT && token_type<=TYPE_STRING)){
+/**/			printf("SSSSSSSSSSSSSss\n");
+				if (is_err(set_type_promene(token_type))!=NO_ERROR){
+/**/				return set_type_promene(token_type);
+/**/			}
+/**/		}
+/**/		else if (token_type==TYPE_IDENTIFIER){
+/**/			int IDecko=zjisti_co_je_id();
+/**/			if (is_err(set_type_promene(IDecko))!=NO_ERROR){
+/**/				return set_type_promene(IDecko);
+/**/			}
+/**/			skipni_get_token=true;
+/**/
+/*frida*/	}
 	printf("%d\n",p_tok1 );
 	printf("%d\n",top_stack );
 	while(!(top_stack == I_DOLLAR && p_tok1 == I_DOLLAR)){
@@ -451,7 +465,23 @@ int rule_expresion_pusher(){
 				printf("I_DOLLAR na konci\n");
 			}
 			p_tok1 = get_prec_table_index(token_type);
-			
+			remember_token_type=token_type;
+/*frida*/	if (token_type!=TYPE_IDENTIFIER && (token_type>=TYPE_INT && token_type<=TYPE_STRING)){
+/**/			printf("SSSSSSSSSSSSSss\n");
+				if (is_err(set_type_promene(token_type))!=NO_ERROR){
+/**/				return set_type_promene(token_type);
+/**/			}
+/**/		}
+/**/		else if (token_type==TYPE_IDENTIFIER){
+/**/			int IDecko=zjisti_co_je_id();
+/**/			if (is_err(set_type_promene(IDecko))!=NO_ERROR){
+/**/				return set_type_promene(IDecko);
+/**/			}
+				printf("IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIiii\n");
+/**/			skipni_get_token=true;
+/**/
+/*frida*/	}
+
 		}else if(prior[top_stack][p_tok1]==R){//Redukuji
 			if(top_of_stack_prepared_for_reduction(&stack)){
 				printf("redukuji :D stack:%d expr:%d\n",top_stack,p_tok1);
@@ -462,6 +492,42 @@ int rule_expresion_pusher(){
 			
 		}else if(prior[top_stack][p_tok1]==E){
 			printf("Equal :D\n");
+
+			SPush(&stack, p_tok1);
+
+			if(remember_token_type == I_DOLLAR){
+				remember_token_type = END_OF_FILE;
+				printf("ANO\n");
+				return ERROR_IDK;
+				break;
+			}else if (set_token_and_return() == END_OF_FILE || token_type == THEN ||
+/*frida*/		 token_type == DO || token_type== END_OF_LINE || token_type == COMMA){
+
+				token_type = I_DOLLAR;
+				printf("I_DOLLAR na konci\n");
+			}
+			p_tok1 = get_prec_table_index(token_type);
+			remember_token_type=token_type;
+/*frida*/	if (token_type!=TYPE_IDENTIFIER && (token_type>=TYPE_INT && token_type<=TYPE_STRING)){
+/**/			if (is_err(set_type_promene(token_type))!=NO_ERROR){
+/**/				return set_type_promene(token_type);
+/**/			}
+/**/		}
+/**/		else{
+/**/			int IDecko=zjisti_co_je_id();
+/**/			if (is_err(set_type_promene(IDecko))!=NO_ERROR){
+/**/				return set_type_promene(IDecko);
+/**/			}
+/**/			skipni_get_token=true;
+/**/
+/*frida*/	}
+
+			if(top_of_stack_prepared_for_reduction(&stack)){
+				printf("equal :D stack:%d expr:%d\n",top_stack,p_tok1);
+			}else{
+				printf("FATAL ERROR WHEN EQUAL\n");
+				return ERROR_IDK;
+			}
 		}
 
 
@@ -511,10 +577,11 @@ int zjisti_co_je_id(){
 					last_id_func=true;
 					uzavorkovana_funkce=false;
 					volani_func++;
-					result = rule_param_counter();
-
-					if(is_err(result)!=NO_ERROR){
-						return result;//musí se volat error je blbost abychom vraceli error, když chceme vlastně jenom typ
+					if (token_type==LEFT_BRACKET || (token_type<=TYPE_IDENTIFIER && token_type>=TYPE_STRING)){
+						result = rule_param_counter();
+						if(is_err(result)!=NO_ERROR){
+							return result;//musí se volat error je blbost abychom vraceli error, když chceme vlastně jenom typ
+						}
 					}
 					data->pocet_parametru=pocitac_param_u_call;
 					pocitac_param_u_call = temp1;
@@ -566,11 +633,12 @@ int zjisti_co_je_id(){
 					last_id_func=true;
 					uzavorkovana_funkce=false;
 					volani_func++;
-					result = rule_param_counter();
-
-					if(is_err(result)!=NO_ERROR){
-						return result;//musí se volat error je blbost abychom vraceli error, když chceme vlastně jenom typ
-					}
+					if (token_type==LEFT_BRACKET || (token_type<=TYPE_IDENTIFIER && token_type>=TYPE_STRING)){
+						result = rule_param_counter();
+						if(is_err(result)!=NO_ERROR){
+							return result;//musí se volat error je blbost abychom vraceli error, když chceme vlastně jenom typ
+						}
+					}	
 					if(pocitac_param_u_call!=0){
 						return ERROR_5;//musí se volat error je blbost abychom vraceli error, když chceme vlastně jenom typ
 						
